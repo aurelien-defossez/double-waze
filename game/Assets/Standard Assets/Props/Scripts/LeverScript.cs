4cf05abbb3750d76d6d6641d349040a7d3bb9aaf
Aurelien Defossez <defossez.aurelien@gmail.com>
2014-01-25 14:50:13 +0100

    Lever added and can be activated with E.

---
+++ game/Assets/Standard Assets/Props/Scripts/LeverScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeverScript : MonoBehaviour {
+	private bool activated = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerStay(Collider other) {
+		if (Input.GetButton("Action")) {
+			if (!gameObject.animation.isPlaying) {
+				activated = !activated;
+				gameObject.animation.Play(activated ? "activate" : "deactivate");
+			}
+		}
+	}
+}
