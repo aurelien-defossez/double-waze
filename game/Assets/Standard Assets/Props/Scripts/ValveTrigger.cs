092ee6569e774ceb051c9ba39a6a4b68efa76827
Aurelien Defossez <defossez.aurelien@gmail.com>
2014-01-25 10:52:51 +0100

    Valve added, pressing E in front of it will make it rotate.

---
+++ game/Assets/Standard Assets/Props/Scripts/ValveTrigger.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ValveTrigger : MonoBehaviour {
+	const float rotationSpeed = 45f;
+
+	private bool pushed = false;
+	private GameObject valve;
+
+	// Use this for initialization
+	void Start () {
+		valve = GameObject.Find ("Valve");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pushed) {
+			valve.transform.RotateAround (
+				valve.transform.position,
+				Vector3.back,
+				rotationSpeed * Time.deltaTime
+			);
+		}
+	}
+	
+	void OnTriggerEnter(Collider other) {
+
+	}
+	
+	void OnTriggerStay(Collider other) {
+		if (Input.GetButton("Action")) {
+			StartPush();
+		} else {
+			StopPush();
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		StopPush ();
+	}
+	
+	void StartPush() {
+		if (!pushed) {
+			pushed = true;
+		}
+	}
+	
+	void StopPush() {
+		if (pushed) {
+			pushed = false;
+		}
+	}
+}
