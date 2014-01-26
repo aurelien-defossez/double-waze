962b6fc52866b4e91bda7ff7a5de3a626c3351fe
Nicolas Dossou-Gbété <nicolas.dossougbete@gmail.com>
2014-01-26 09:29:29 +0100

    Level 5, ladder fix

---
+++ game/Assets/Scripts/GroupProperty.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroupProperty : MonoBehaviour {
+
+	public int group;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
